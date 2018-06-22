8f0bd3d2a72d5d8d4c6b8afc99056baf1724e9df
Melissa Pabst <melissapabst@gmail.com>
2018-06-22 01:56:02 -0500

    create controller handler to render add category form

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,11 +1,13 @@
 package org.launchcode.controllers;
 
 
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("category")
@@ -15,7 +17,7 @@ public class CategoryController {
     private CategoryDao categoryDao;
 
     //"" = path "category"
-    @RequestMapping("")
+    @RequestMapping(value = "")
     public String index(Model model) {
 
         model.addAttribute("categories", categoryDao.findAll());
@@ -23,5 +25,17 @@ public class CategoryController {
 
         return "category/index";
     }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCategoryForm(Model model) {
+
+        //create a new Category object using default constructor, pass into the view with key "category"
+        model.addAttribute(new Category());
+        model.addAttribute("title", "Add Category");
+
+        return "category/add";
+
+    }
+
 
 }
