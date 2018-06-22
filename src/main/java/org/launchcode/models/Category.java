2092047527a3af2a749d70174da8301e567be8f5
Melissa Pabst <melissapabst@gmail.com>
2018-06-21 19:00:39 -0500

    set up new category model

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,37 @@
+package org.launchcode.models;
+
+
+import com.sun.istack.internal.NotNull;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    //default (no-argument) constructor
+    public Category() {
+    }
+
+    public Category (String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
