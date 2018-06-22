e9188b8954a19f757b10fcb7719c4bfb8ae930e9
Melissa Pabst <melissapabst@gmail.com>
2018-06-22 02:01:59 -0500

    add handler to process add category form, save category to dao

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -6,8 +6,12 @@ import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("category")
@@ -37,5 +41,17 @@ public class CategoryController {
 
     }
 
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCategoryForm(Model model, @ModelAttribute @Valid Category category, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Category");
+            return "category/add";
+        }
+
+        categoryDao.save(category);
+        return "redirect:";
+
+    }
 
 }
