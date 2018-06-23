71dbdf88808670184416d4dcd726fef61fb69b7f
Melissa Pabst <melissapabst@gmail.com>
2018-06-22 21:55:19 -0500

    process add menu

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,12 +1,18 @@
 package org.launchcode.controllers;
 
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -22,10 +28,34 @@ public class MenuController {
     @RequestMapping(value = "")
     public String index(Model model) {
 
+
         model.addAttribute("menus", menuDao.findAll());
         model.addAttribute("title", "All Menus");
 
         return "menu/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String addMenu(Model model) {
+
+        model.addAttribute(new Menu());
+        model.addAttribute("title", "Add New Menu");
+
+        return "menu/add";
+
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processMenu(@ModelAttribute @Valid Menu newMenu, Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add New Menu");
+            return "menu/add";
+        }
+
+        menuDao.save(newMenu);
+
+        return "redirect:view/" + newMenu.getId();
     }
 }
 
