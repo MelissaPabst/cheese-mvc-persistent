c11221e51be7756cce719254cf0ab821b2c2eedc
Melissa Pabst <melissapabst@gmail.com>
2018-06-22 20:37:01 -0500

    create menuDao interface

--- src/main/java/org/launchcode/models/data/MenuDao.java
+++ src/main/java/org/launchcode/models/data/MenuDao.java
@@ -1,14 +1,13 @@
 package org.launchcode.models.data;
 
-
-import org.launchcode.models.Category;
+import org.launchcode.models.Menu;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import javax.transaction.Transactional;
 
+//This dao allows us to access Menu objects via the data layer from within our controllers
 
 @Repository
 @Transactional
-public interface CategoryDao extends CrudRepository<Category, Integer> {
+public interface MenuDao extends CrudRepository<Menu, Integer> {
 }
